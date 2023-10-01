3997e7d3e890b0dc6ddff77f96345a97a218129c
Anna Schrandt-Pawłowska <pawlowski.t.93@gmail.com>
2023-10-01 20:16:45 +0200

    first changes

--- src/js/api.js
+++
@@ -1,9 +0,0 @@
-`use strict`;
-import Notiflix from 'notiflix';
-import axios from 'axios';
-
-const searchInput = document.querySelector('.search-form input');
-
-const apiKey = `39664886-734d85d446af9c48bd55da1f3`;
-const userInput = searchInput.textContent;
-const url = `https://pixabay.com/api/?key=${apiKey}&q=${userInput}`;
--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,74 @@
 `use strict`;
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+import 'notiflix/dist/notiflix-3.2.6.min.css';
+import axios from 'axios';
+
+const moreBtn = document.querySelector(".load-more");
+const gallery = document.querySelector(".gallery");
+const searchForm = document.querySelector("#search-form")
+
+moreBtn.style.display = "none";
+
+const apiKey = `39664886-734d85d446af9c48bd55da1f3`;
+// const userInput = searchForm.currentTarget.value;
+const pageCount = 1;
+let userInput = ``;
+const url = `https://pixabay.com/api/`;
+
+const options = {
+  params: {
+    key: apiKey,
+    q: userInput,
+    image_type: "photo",
+    orientation: "horizontal",
+    safesearch: true,
+    page: pageCount,
+    per_page: 40
+  }
+}
+
+const fetchResults = async () => {
+  try {
+    const response = await axios(url, options)
+    console.log(response)
+    console.log("działa")
+  } catch {
+    Notiflix.Notify.faliure(error)
+  }
+  
+}
+
+const createGallery = images => {
+    const markup = images.map(image => `<div class="photo-card">
+  <a class="photo-card__link" href="${image.largeImageURL}">
+  <img class="photo-card__image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${image.likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${image.views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${image.comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${image.downloads}
+    </p>
+  </div>
+</div>`)
+        .join(``);
+    gallery.innerHTML += markup;
+    const lightbox = new SimpleLightbox('.gallery a');
+  lightbox.refresh();
+}
+
+searchForm.addEventListener("submit", event => {
+  event.preventDefault();
+  const inputValue = event.currentTarget.elements.searchQuery.value;
+  userInput = inputValue;
+  fetchResults();
+  console.log(userInput)
+})
